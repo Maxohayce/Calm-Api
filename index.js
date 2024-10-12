a6c6efdff1afd91e45dcb43302f194296e1d9d33
Maxohayce <devmax.corl@gmail.com>
2024-10-12 16:19:25 +0100

    new update

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => {
   res.send("server is running");
 });
@@ -30,7 +36,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
